8d51281afd87e9b73ce12ada20b33375657f2fc3
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-09 16:36:10 +0530

    router and Navbar imported in app

--- frontend/easymoney/src/App.jsx
+++ frontend/easymoney/src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import Navbar from './componants/Navbar/Navbar.jsx';
 
 function App() {
 
 
   return (
-    <>
+    <> 
+    <BrowserRouter>
+        <Navbar />
+
+    </BrowserRouter>
  
     </>
   )
