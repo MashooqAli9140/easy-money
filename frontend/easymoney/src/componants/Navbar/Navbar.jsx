388c427d2ff66f97046fcd5eb6e9f5ab4b1d8e00
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-09 17:17:13 +0530

    Navbar logo created and css file created

--- frontend/easymoney/src/componants/Navbar/Navbar.jsx
+++ frontend/easymoney/src/componants/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Logo from "/images/Logo.png"
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-        <h1> easy money </h1>
+    <div id='nav-main-box' style={{ padding:"0 100px 0 100px", width:'100%' ,backgroundColor:"#212426"}}>
+      <div id='navleft' style={ { width:"250px" , backgroundColor:"#212426"}}>
+          <img style={{ width:'100%'}} src= {Logo} alt="Logo" />
+      </div>
+      <div id='navright'>
+          <button> Login </button>
+          <button> SignUp </button>
+      </div>
     </div>
   )
 }
