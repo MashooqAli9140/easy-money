9adfaa0a5123d82efbdb25bc47b3bf6a8672e792
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-11 15:06:41 +0530

    home page hero section elements added and updated

--- frontend/easymoney/src/componants/Home_Hero/Home_hero.jsx
+++ frontend/easymoney/src/componants/Home_Hero/Home_hero.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './Home_hero.css';
+import Grow from "/images/Grow.png"
 
 const Home_hero = () => {
   return (
-    <div>
-        <div>
-            <h1> Welcome to Hero section </h1>
+    <div id='home-hero'>
+        <div id='home-hero-left'>
+            <h1> Grow wealth. </h1> 
+            <h1> Achieve Goals. </h1>
+            <h1> Invest in the best mutual funds. </h1>
+            <div style={{ width:"60%", marginTop:"10px"}}>
+                <h5 style={{ fontWeight:"100"}}>
+                Wealth is not just about money. It's about what all you can do with it. It is having your own story of progress. And living it every single day. So go ahead, imagine a future you want to shape.
+                </h5>
+            </div>
+            <div id='start-btn-div'>
+                <button style={{ background:'none', borderRadius:"25px", width:"100%", padding:"12px 8px 12px 8px", color:"white", border:"1px solid #BAFF2F",outline:"none"}}> <a href="/"> Start Your Journey </a></button>
+            </div>
+        </div>
+        <div id='home-hero-right' >
+            <div id='grow-icon-div'>
+                  <img style={{ width:"100%"}} src= {Grow} alt="grow_icon" />
+            </div>
         </div>
       
     </div>
