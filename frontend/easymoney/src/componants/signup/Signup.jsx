dbfa0642ad6dafd6813b5a85a33ee45647982fb3
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 01:50:45 +0530

    signup text added

--- frontend/easymoney/src/componants/signup/Signup.jsx
+++ frontend/easymoney/src/componants/signup/Signup.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Signup = () => {
   return (
     <div>
-      <div>
+      <div style={{ color:"white", padding:"20px 20px 20px 20px", backgroundColor:"#00B852", margin:"0 auto 0", textAlign:"center",fontSize:"25px"}}>
         <h1> Start your investing journey today. </h1>
       </div>
     </div>
