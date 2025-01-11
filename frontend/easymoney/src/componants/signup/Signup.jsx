7f1646f3c6cefdd6a38f01f0f2cb6a1e76ed4949
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 01:46:39 +0530

    signup bottom button created

---
+++ frontend/easymoney/src/componants/signup/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <div>
+        <h1> Start your investing journey today. </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
