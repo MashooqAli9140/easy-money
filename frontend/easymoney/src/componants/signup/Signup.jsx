21867291b2bb4260d502b8d1a92ddb16978b0a17
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 02:09:43 +0530

    sign up bottom btn updated

--- frontend/easymoney/src/componants/signup/Signup.jsx
+++ frontend/easymoney/src/componants/signup/Signup.jsx
@@ -5,7 +5,11 @@ const Signup = () => {
     <div>
       <div style={{ color:"white", padding:"20px 20px 20px 20px", backgroundColor:"#00B852", margin:"0 auto 0", textAlign:"center",fontSize:"25px"}}>
         <h1> Start your investing journey today. </h1>
+        <div style={{ width:"240px",maxWidth:"240px", margin:"0 auto 0", marginTop:"20px"}}>
+        <button style={{ cursor:"pointer", borderRadius:"25px", border:"none",outline:"none",background:"white",fontSize:"18ox",fontWeight:"500",padding:"10px 15px 10px 15px", width:"100%"}}> Sign up now </button>
       </div>
+      </div>
+
     </div>
   )
 }
