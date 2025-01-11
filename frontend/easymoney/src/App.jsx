962fa055357964fbb1626cb26792612d83e7aba7
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 01:47:52 +0530

    signup button imported in app

--- frontend/easymoney/src/App.jsx
+++ frontend/easymoney/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import Navbar from './componants/Navbar/Navbar.jsx';
 import Home_hero from './componants/Home_Hero/Home_hero.jsx';
 import Home_Hero_2 from './componants/Home_Hero/Home_Hero_2.jsx';
+import Signup from './componants/signup/Signup.jsx';
 
 function App() {
 
@@ -16,6 +17,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path='/'  element={ <Home_Hero_2/> } />
+        </Routes>
+        <Routes>
+          <Route path='/'  element={ <Signup/> } />
         </Routes>
 
     </BrowserRouter>
