9e2c7ddd0d304142c1ebf050bb20c04f5cffaa6a
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-11 11:40:27 +0530

    making NAV responsive

--- frontend/easymoney/src/componants/Navbar/Navbar.jsx
+++ frontend/easymoney/src/componants/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <div id='nav-main-box' style={{ padding:"0 100px 0 100px", width:'100%' ,backgroundColor:"#212426"}}>
-      <div id='navleft' style={ { width:"250px" , backgroundColor:"#212426"}}>
+      <div id='navleft' style={ { width:"250px", maxwidth:"250px" , backgroundColor:"#212426"}}>
           <img style={{ width:'100%'}} src= {Logo} alt="Logo" />
       </div>
       <div id='nav-mid'>
@@ -17,6 +17,9 @@ const Navbar = () => {
       <div id='navright'>
           <button id='login-btn'> Login </button>
           <button id='signup-btn' > SignUp </button>
+      </div>
+      <div id='nav-right-menu'>
+
       </div>
     </div>
   )
