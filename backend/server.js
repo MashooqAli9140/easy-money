418c41d4fab3f24e55cb312ac7896992f2af0dae
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 18:45:54 +0530

    mongoose intalled

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+
 
 
 const PORT = process.env.PORT || 4500;
-
-
 app.listen( PORT , ()=> {
     console.log( `server running on PORT${PORT} `)
 })
