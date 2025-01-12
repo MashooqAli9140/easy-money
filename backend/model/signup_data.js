e1469eb558433e138378f0cde59c2fe144401c24
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 19:09:12 +0530

    signup schema created

---
+++ backend/model/signup_data.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const signup_data = new Schema( {
+     name:{ type:String , required:true },
+     email:{ type:String , required:true },
+     password:{ type:String , required: true },
+     mobile_number:{ type:Number , required: true }
+})
