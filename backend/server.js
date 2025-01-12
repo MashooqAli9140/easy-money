4ecd79fa0b4e19cc0eea21ba9aef68ac9ba58234
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 18:43:10 +0530

    backend created and express,cors,nodemon installed

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+const PORT = process.env.PORT || 4500;
+
+
+app.listen( PORT , ()=> {
+    console.log( `server running on PORT${PORT} `)
+})
