a3cb2760a315a41f87f68e96c7cccafd83d777ce
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 19:13:27 +0530

    signup_data schema imported in server

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const connectDB = require('./config/db.js');
-
+const signup_data = require('./model/signup_data.js')
 
 
 //DATABCE CONNECTED
