62e9c745dd8588f695694e45a30ce66fdb1d291d
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 14:08:28 +0530

    footer created

---
+++ frontend/easymoney/src/componants/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            <div> <p> Top Searched Schemes: </p> </div>
+            <div> <p> Quant Small Cap FundQuant Infrastructure FundQuant ELSS Tax Saver FundAxis Small Cap FundQuant Mid Cap FundNippon India Small Cap Fund </p> </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
