64c79f41591ddd815cc969fd0405f6fed8ee6937
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 16:42:26 +0530

    signup page updated

--- frontend/easymoney/src/Pages/Signup_page/Signup_page.jsx
+++ frontend/easymoney/src/Pages/Signup_page/Signup_page.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Signup_page = () => {
   return (
     <div>
-      
+        <div>
+             <h1> welcome to signup page </h1>
+        </div>
     </div>
   )
 }
