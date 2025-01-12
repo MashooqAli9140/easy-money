0265fc272d4f4fbe5f8af6a5f8b4a9dcc27d44c4
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 19:12:07 +0530

    signup user schema updated

--- backend/model/signup_data.js
+++ backend/model/signup_data.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 const signup_data = new Schema( {
      name:{ type:String , required:true },
      email:{ type:String , required:true },
-     password:{ type:String , required: true },
+     password:{ type:String , required:true },
      mobile_number:{ type:Number , required: true }
-})
+},
+  { collection:"user-signup-data"}
+)
+
+module.exports = mongoose.model('signup_data' , signup_data )
