b6b3a8fba5d60462a4ce66656ca1741d92ea470a
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-12 14:10:07 +0530

    footer imported in app

--- frontend/easymoney/src/App.jsx
+++ frontend/easymoney/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './componants/Navbar/Navbar.jsx';
 import Home_hero from './componants/Home_Hero/Home_hero.jsx';
 import Home_Hero_2 from './componants/Home_Hero/Home_Hero_2.jsx';
 import Signup from './componants/signup/Signup.jsx';
+import Footer from './componants/footer/Footer.jsx';
 
 function App() {
 
@@ -20,6 +21,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path='/'  element={ <Signup/> } />
+        </Routes>
+        <Routes>
+          <Route path='/'  element={ <Footer/> } />
         </Routes>
 
     </BrowserRouter>
