ea6f6bd9cf5d77cc3272e9a10617535b8ec68341
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-14 22:08:59 +0530

    dashboard updated

--- frontend/easymoney/src/Pages/User_Dashboard/Dashboard.jsx
+++ frontend/easymoney/src/Pages/User_Dashboard/Dashboard.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useGlobleContext } from '../../componants/GlobleContext/GlobleContext';
+import './Dashboard.css';
 
 const Dashboard = () => {
   const { id } = useParams();
   const { login , setLogin } = useGlobleContext();
-  if( id ) setLogin(true);
+  if( id ){
+    setLogin(true)
+  };
   
   return (
-    <div>
-         <div> 
-            <h1> Welcome to Dashboard {id} and id from context { id } </h1>
-         </div>
+    <div id='dashboard-hero'> 
+          <div id='dashboard-hero-content'>
+                 <div>
+                    <h1> hello User nice to meet you! </h1>
+                    <p>Welcome aboard. Start investing or upload your external wealth to get started.</p>
+                 </div>
+                 <div style={{marginTop:'20px', flexGrow:"grow",display:"flex", gap:'10px', backgroundColor:"red", padding:"20px 0px 20px 0px"}}>
+                         <div style={{ backgroundColor:"green" , width:'100%'}} >
+                            <h3> left </h3>
+                         </div>
+                         <div style={{ backgroundColor:"blue", width:'100%'}} >
+                             <h3> right </h3>
+                         </div>
+                 </div>
+          </div>
     </div>
   )
 }
