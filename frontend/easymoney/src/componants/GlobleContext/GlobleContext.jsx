414d291af106527e90e9f89762aab3828d5f4c2f
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-15 02:59:04 +0530

    get user data req updated and dashboard updated

--- frontend/easymoney/src/componants/GlobleContext/GlobleContext.jsx
+++ frontend/easymoney/src/componants/GlobleContext/GlobleContext.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createContext , useContext , useState } from "react";
 import { useParams } from 'react-router-dom';
 import axios from 'axios'
@@ -11,21 +11,25 @@ export const GlobleProvider = ( {children} ) => {
 
 
        //create function for getting user data from backend
-       async function GetUserData( ) {
-             try {
-                const response = await axios.get(`http://localhost:3000/get-user-data/${login_user}`,{
+       useEffect( () => {
+        async function GetUserData( ) {
+            try {
+               const response = await axios.get(`http://localhost:3000/get-user-data/${login_user}`,{
 
-                })
-                return response.status(200).json({'msge':"user data get success"})
-             } catch (error) {
-                console.log(error.msge , "error while getting the data")
-                alert("user data not get");
-             }
-       }
+               })
+               return response.status(200).json({'msge':"user data get success"})
+            } catch (error) {
+               console.log(error.msge , "error while getting the data")
+               alert("user data not get");
+            }
+      }
+      GetUserData()
+    },login_user)
+
 
        
     return(
-        <GlobleContext.Provider value={{ login , setLogin , login_user , setlogin_user , GetUserData }}>
+        <GlobleContext.Provider value={{ login , setLogin , login_user , setlogin_user}}>
             { children }
         </GlobleContext.Provider>
     )
