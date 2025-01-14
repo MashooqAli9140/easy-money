fe653c7a3a32241eefda5f9135dd38537cfd75e3
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-15 01:27:30 +0530

    dashboard hero section created

--- frontend/easymoney/src/Pages/User_Dashboard/Dashboard.jsx
+++ frontend/easymoney/src/Pages/User_Dashboard/Dashboard.jsx
@@ -1,33 +1,65 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useGlobleContext } from '../../componants/GlobleContext/GlobleContext';
-import './Dashboard.css';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useGlobleContext } from "../../componants/GlobleContext/GlobleContext";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   const { id } = useParams();
-  const { login , setLogin } = useGlobleContext();
-  if( id ){
-    setLogin(true)
-  };
-  
-  return (
-    <div id='dashboard-hero'> 
-          <div id='dashboard-hero-content'>
-                 <div>
-                    <h1> hello User nice to meet you! </h1>
-                    <p>Welcome aboard. Start investing or upload your external wealth to get started.</p>
-                 </div>
-                 <div style={{marginTop:'20px', flexGrow:"grow",display:"flex", gap:'10px', backgroundColor:"red", padding:"20px 0px 20px 0px"}}>
-                         <div style={{ backgroundColor:"green" , width:'100%'}} >
-                            <h3> left </h3>
-                         </div>
-                         <div style={{ backgroundColor:"blue", width:'100%'}} >
-                             <h3> right </h3>
-                         </div>
-                 </div>
-          </div>
-    </div>
-  )
-}
+  const { login, setLogin } = useGlobleContext();
+  if (id) {
+    setLogin(true);
+  }
 
-export default Dashboard
+  return (
+    <div id="dashboard-hero">
+
+      <div id="dashboard-hero-header">
+           <h1> hello User nice to meet you! </h1>
+          <p>
+            Welcome aboard. Start investing or upload your external wealth to
+            get started.
+          </p>
+
+        <div id="hero-card-box" style={{ marginTop: "20px",padding: "20px 0px 20px 0px" }} >
+
+          {/* //left part */}
+          <div id="dashboard-hero-left" style={{width: "100%" }}>
+          <div style={{ padding:"10px 10px 10px 10px" , borderRadius:"25px",border:"1px solid lightgrey"}}>
+          <div style={{ padding:"10px 10px 10px 10px"}}>
+            <h1 style={{ fontWeight:"500"}}> Start investing </h1>
+            <p> Invest in the right funds that's personalised for you </p>
+          </div>
+
+            <div style={{ padding:"10px 10px 10px 10px"}}><a href="">
+              <button id="continue-btn"> Continue </button>
+              </a>
+            </div>
+            </div>
+          </div>
+
+       {/* //right part */}
+      <div id="dashboard-hero-left" style={{width: "100%" }}>
+        <div style={{ padding:"10px 10px 10px 10px" , borderRadius:"25px",border:"1px solid lightgrey"}}>
+          <div style={{ padding:"10px 10px 10px 10px"}}>
+            <h1 style={{ fontWeight:"500"}} > Start an SIP </h1>
+            <p> Invest in the right funds that's personalised for you </p>
+          </div>
+
+            <div style={{ padding:"10px 10px 10px 10px"}}><a href="">
+              <button id="continue-btn"> Start SIP  </button>
+              </a>
+            </div>
+        </div>
+      </div>
+        {/* //right part */}
+
+        </div>
+      </div>
+
+
+      
+    </div>
+  );
+};
+
+export default Dashboard;
