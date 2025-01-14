8e0d91a8d625f7b9a9a82b9666c51a1e4ff36771
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-14 14:49:05 +0530

    user dashboard page created

---
+++ frontend/easymoney/src/Pages/User_Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+         <div>
+            <h1> Welcome to Dashboard </h1>
+         </div>
+    </div>
+  )
+}
+
+export default Dashboard
