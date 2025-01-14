606382d06692f0521b1bd245ed8a59b4e21985d0
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-14 16:03:55 +0530

    GLobel Provider imported in main.jsx

--- frontend/easymoney/src/main.jsx
+++ frontend/easymoney/src/main.jsx
@@ -3,9 +3,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { GlobleProvider } from './componants/GlobleContext/GlobleContext.jsx';
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
+
+    <GlobleProvider>
     <App />
+    </GlobleProvider>
+    
   </StrictMode>
 );
