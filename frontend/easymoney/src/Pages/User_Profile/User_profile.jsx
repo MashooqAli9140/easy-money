d293d06dbfcdd1862d86ae718e505bc7c5a344e2
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-17 03:40:57 +0530

    user profile page created

---
+++ frontend/easymoney/src/Pages/User_Profile/User_profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from "../../componants/Navbar/Navbar";
+
+const User_profile = () => {
+  return (
+    <>
+    <Navbar />
+    <div>
+         <div>
+            <h1> welcome to user profile </h1>
+         </div>
+    </div>
+    </>
+  )
+}
+
+export default User_profile
