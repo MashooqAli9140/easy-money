216061ff26229a3ebdd48177db6ab206624887f2
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-16 13:15:13 +0530

    stock page created

---
+++ frontend/easymoney/src/Pages/Stocks_page/Stocks_page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from "../../componants/Navbar/Navbar";
+
+const Stocks_page = () => {
+
+  return (
+ <>
+ <Navbar />
+    <div>
+        <div>
+            welcome to stock page
+        </div>
+    </div>
+
+</>
+
+  )
+}
+
+export default Stocks_page
