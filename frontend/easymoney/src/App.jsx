f7f8673a3ce438646ed2a5bfb3bdecf46141be84
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-17 03:45:16 +0530

    user profile imported in app and routes updated

--- frontend/easymoney/src/App.jsx
+++ frontend/easymoney/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import { BrowserRouter , Routes , Route, Router } from 'react-router-dom';
 // import Navbar from './componants/Navbar/Navbar.jsx';
 import Home_hero from './componants/Home_Hero/Home_hero.jsx';
 import Home_Hero_2 from './componants/Home_Hero/Home_Hero_2.jsx';
@@ -10,6 +10,7 @@ import Dashboard from './Pages/User_Dashboard/Dashboard.jsx';
 import { GlobleProvider } from './componants/GlobleContext/GlobleContext.jsx';
 import Mutual_fund from './Pages/Mutual_fund_page/Mutual_fund.jsx';
 import Stocks_page from './Pages/Stocks_page/Stocks_page.jsx';
+import User_profile from './Pages/User_Profile/User_profile.jsx';
 
 function App() {
 
@@ -20,29 +21,16 @@ function App() {
 
         <Routes>
           <Route path='/'  element={ <Home_hero/> } />
-        </Routes>
-        <Routes>
           <Route path='/'  element={ <Home_Hero_2/> } />
-        </Routes>
-        <Routes>
           <Route path='/'  element={ <Signup/> } />
-        </Routes>
-        <Routes>
           <Route path='/'  element={ <Footer/> } />
-        </Routes>
-        <Routes>
           <Route path='/signup'  element={ <Signup_page/> } />
-        </Routes>
-        <Routes>
           <Route path='/dashboard/:id'  element={ <Dashboard/> } />
-        </Routes>
-        <Routes>
           <Route path='/dashboard/mutualfunds/:id'  element={ <Mutual_fund/> } />
-        </Routes>
-        <Routes>
           <Route path='/dashboard/stocks/:id'  element={ <Stocks_page/> } />
+          <Route path='/dashboard/user-profile/:id'  element={ <User_profile/> } />
         </Routes>
-        
+ 
     </BrowserRouter>
 </GlobleProvider>
  
