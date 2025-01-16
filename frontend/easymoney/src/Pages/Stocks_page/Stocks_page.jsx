58253d50bb1997595feee0caa232bd782e9f45fa
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-16 15:45:31 +0530

    stocks data access success from mongodb

--- frontend/easymoney/src/Pages/Stocks_page/Stocks_page.jsx
+++ frontend/easymoney/src/Pages/Stocks_page/Stocks_page.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 import Navbar from "../../componants/Navbar/Navbar";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Stocks_page = () => {
+      const [ CompanyData , setCompanyData ] = useState([])
+      const [ loading ,setloading ] = useState(true);
+      const [error , setError ] = useState(null);
+
+      useEffect(() => {
+        const fetchStocks = async () => {
+          try {
+            const response = await axios.get(`http://localhost:3000/get-stock-data`)
+            setCompanyData( response.data.data[0] );
+            return response.status
+          } catch (err) {
+            console.error("Error fetching data:", err.message);
+          }
+          finally{
+            setloading(false);
+          }
+        };
+        fetchStocks();
+      }, []);
+      if (loading) return <h1>Loading data for top 50 companies...</h1>;
+      if (error) return <h1>Error: {error}</h1>;
+      console.log( "data is this-->", CompanyData );
 
   return (
  <>
