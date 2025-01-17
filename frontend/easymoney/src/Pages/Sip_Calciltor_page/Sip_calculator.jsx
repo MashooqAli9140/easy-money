bbe13c4bbd069c11ad49d145aced66c842d55255
MashooqAli9140 <Faizsde9140@gmail.com>
2025-01-18 03:06:50 +0530

    sip calcultor page created

---
+++ frontend/easymoney/src/Pages/Sip_Calciltor_page/Sip_calculator.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../../componants/Navbar/Navbar'
+
+const Sip_calculator = () => {
+  return (
+    <>
+    <Navbar/>
+    <div>
+         <div>
+            <h1> welcome to sip  calcultor </h1>
+         </div>
+    </div>
+    </>
+  )
+}
+
+export default Sip_calculator
